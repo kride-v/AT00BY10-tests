a70320cad7630ba13f96720e9156da31d9d53910
Kristian V <258425640+kride-v@users.noreply.github.com>
2026-04-05 00:48:49 +0300

    Remove jest.config.js to resolve conflict

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-// jest.config.js
-export default {
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageReporters: ["lcov", "text"],
-  collectCoverageFrom: ["src/**/*.js"], // Tämä kertoo Jestille, että src-kansion kaikki .js-tiedostot seurataan
-};
