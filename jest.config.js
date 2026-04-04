c0890d1f40e8e4044bbaa1146a9bbef95ffd5468
kride-v <258425640+kride-v@users.noreply.github.com>
2026-04-04 22:51:58 +0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ export default {
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageReporters: ["lcov", "text"],
+  collectCoverageFrom: ["src/**/*.js"], // Tämä kertoo Jestille, että src-kansion kaikki .js-tiedostot seurataan
 };
